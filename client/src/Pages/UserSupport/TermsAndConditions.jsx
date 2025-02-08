d2b6779409da98a2918681f5d36696d218d68109
AMISHA_KUMARI <amishak9824@gmail.com>
2025-02-08 22:02:27 +0530

    added TermsandConditions

--- client/src/Pages/UserSupport/TermsAndConditions.jsx
+++ client/src/Pages/UserSupport/TermsAndConditions.jsx
@@ -2,68 +2,59 @@ import React from 'react';
 
 const TermsAndConditions = () => {
     return (
-        <div>
-            <h1>Terms and Conditions</h1>
-            <p>Welcome to our Terms and Conditions page. Please read the following terms carefully.</p>
-            <section>
-                <h2>Introduction</h2>
-                <p>These terms and conditions outline the rules and regulations for the use of our website.</p>
-            </section>
-            <section>
-                <h2>Intellectual Property Rights</h2>
-                <p>Other than the content you own, under these terms, we own all the intellectual property rights and materials contained in this website.</p>
-            </section>
-            <section>
-                <h2>Restrictions</h2>
-                <p>You are specifically restricted from all of the following:</p>
-                <ul>
-                    <li>Publishing any website material in any other media</li>
-                    <li>Selling, sublicensing and/or otherwise commercializing any website material</li>
-                    <li>Publicly performing and/or showing any website material</li>
-                    <li>Using this website in any way that is or may be damaging to this website</li>
-                    <li>Using this website in any way that impacts user access to this website</li>
-                    <li>Using this website contrary to applicable laws and regulations, or in any way may cause harm to the website, or to any person or business entity</li>
-                    <li>Engaging in any data mining, data harvesting, data extracting or any other similar activity in relation to this website</li>
-                    <li>Using this website to engage in any advertising or marketing</li>
-                </ul>
-            </section>
-            <section>
-                <h2>Your Content</h2>
-                <p>In these terms and conditions, "Your Content" shall mean any audio, video text, images or other material you choose to display on this website. By displaying Your Content, you grant us a non-exclusive, worldwide irrevocable, sub-licensable license to use, reproduce, adapt, publish, translate and distribute it in any and all media.</p>
-            </section>
-            <section>
-                <h2>No warranties</h2>
-                <p>This website is provided "as is," with all faults, and we express no representations or warranties, of any kind related to this website or the materials contained on this website. Also, nothing contained on this website shall be interpreted as advising you.</p>
-            </section>
-            <section>
-                <h2>Limitation of liability</h2>
-                <p>In no event shall we, nor any of our officers, directors and employees, be held liable for anything arising out of or in any way connected with your use of this website whether such liability is under contract. We, including our officers, directors and employees shall not be held liable for any indirect, consequential or special liability arising out of or in any way related to your use of this website.</p>
-            </section>
-            <section>
-                <h2>Indemnification</h2>
-                <p>You hereby indemnify to the fullest extent us from and against any and/or all liabilities, costs, demands, causes of action, damages and expenses arising in any way related to your breach of any of the provisions of these terms.</p>
-            </section>
-            <section>
-                <h2>Severability</h2>
-                <p>If any provision of these terms is found to be invalid under any applicable law, such provisions shall be deleted without affecting the remaining provisions herein.</p>
-            </section>
-            <section>
-                <h2>Variation of Terms</h2>
-                <p>We are permitted to revise these terms at any time as we see fit, and by using this website you are expected to review these terms on a regular basis.</p>
-            </section>
-            <section>
-                <h2>Assignment</h2>
-                <p>We are allowed to assign, transfer, and subcontract our rights and/or obligations under these terms without any notification. However, you are not allowed to assign, transfer, or subcontract any of your rights and/or obligations under these terms.</p>
-            </section>
-            <section>
-                <h2>Entire Agreement</h2>
-                <p>These terms constitute the entire agreement between us and you in relation to your use of this website, and supersede all prior agreements and understandings.</p>
-            </section>
-            <section>
-                <h2>Governing Law & Jurisdiction</h2>
-                <p>These terms will be governed by and interpreted in accordance with the laws of the State of [Your State], and you submit to the non-exclusive jurisdiction of the state and federal courts located in [Your State] for the resolution of any disputes.</p>
-            </section>
-        </div>
+        <>
+            <h1 className="p-8 text-4xl font-bold text-gfg-black mb-6 text-center">
+                Terms and Conditions
+            </h1>
+            <div className="bg-white rounded-xl shadow-sm p-8 space-y-6">
+                <div>
+                    <h2 className="text-emerald-600 text-2xl font-semibold tracking-tight mb-2">
+                        Terms of Use
+                    </h2>
+                    <p className="text-gray-700 text-justify">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, dolores adipisci aut nostrum culpa labore rerum officiis excepturi ea inventore consequatur in, nemo natus. Expedita eligendi sequi modi autem magnam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam veritatis voluptates blanditiis sequi iusto maiores at tempore molestias tempora amet saepe distinctio, corporis culpa. Quia maxime perspiciatis veniam quas sequi?
+                    </p>
+                </div>
+                <div>
+                    <h2 className="text-emerald-600 text-2xl font-semibold tracking-tight mb-2">
+                        Conditions of Use
+                    </h2>
+                    <p className="text-gray-700 text-justify">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et consectetur nam sunt ducimus debitis porro illo voluptate? Illum sed temporibus vitae facilis, praesentium placeat nobis neque ipsa, quasi tenetur amet. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus aliquam illo ea officia voluptas, fugiat, distinctio corrupti harum possimus optio expedita dolorum aliquid ipsam, itaque repellat officiis accusantium accusamus vel!
+                    </p>
+                </div>
+                <div>
+                    <h2 className="text-emerald-600 text-2xl font-semibold tracking-tight mb-2">
+                        Privacy Policy
+                    </h2>
+                    <p className="text-gray-700 text-justify">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia accusantium iusto minus repellendus recusandae delectus error fugiat tempore, dolorem maiores pariatur beatae quibusdam perspiciatis distinctio non nesciunt, perferendis molestiae aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora asperiores quis molestiae sapiente nisi illum possimus eius perspiciatis! Similique, voluptates perspiciatis! Quo voluptatum nisi saepe nihil dicta hic, nulla laudantium?
+                    </p>
+                    <div className="p-8 space-y-6">
+                        <div className="flex items-start space-x-2">
+                            <div className="w-2 h-2 bg-gfgsc-green rounded-full mt-2"></div>
+                            <p className="flex-1 text-justify">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi vel nobis tempora itaque tempore consequuntur, beatae natus. Eum corrupti, debitis nostrum corporis, maxime, laudantium ea animi quidem totam soluta eaque! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic earum provident ratione aliquid vitae ab magnam? Distinctio nam eius quod placeat nihil in error facilis dolor officia temporibus? Culpa, a.
+                            </p>
+                        </div>
+
+                        <div className="flex items-start space-x-2">
+                            <div className="w-2 h-2 bg-gfgsc-green rounded-full mt-2"></div>
+                            <p className="flex-1 text-justify">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste reiciendis quasi, id porro ipsum dolores numquam, voluptas praesentium error quo maxime eum. Cum nostrum voluptate fuga, sunt atque illum harum.
+                            </p>
+                        </div>
+
+                        <div className="flex items-start space-x-2">
+                            <div className="w-2 h-2 bg-gfgsc-green rounded-full mt-2"></div>
+                            <p className="flex-1 text-justify">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit ratione officiis facere eligendi cupiditate dignissimos, voluptatibus itaque recusandae sint doloribus quis blanditiis maiores corporis optio dolor deserunt, odio enim sapiente.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
