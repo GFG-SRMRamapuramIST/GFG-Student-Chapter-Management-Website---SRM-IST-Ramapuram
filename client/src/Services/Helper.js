114904aa0f07a76bcb21048d741eaf30c879fca5
VISHAL KUAMR YADAV <vishal100403@gmail.com>
2025-02-09 17:27:26 +0530

    fix: update BACKEND_URL to point to the production server on Vercel

--- client/src/Services/Helper.js
+++ client/src/Services/Helper.js
@@ -1,1 +1,3 @@
-export const BACKEND_URL = "http://localhost:4002";
+//export const BACKEND_URL = "http://localhost:4002";
+export const BACKEND_URL =
+  "https://gfgsc-management-website-srmist-ramapuram-server.vercel.app";
