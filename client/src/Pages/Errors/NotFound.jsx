5164d19a2365dddd02ba2af8250dc3d5f28bf1cb
AMISHA_KUMARI <amishak9824@gmail.com>
2025-02-10 22:14:44 +0530

    404 error page added

--- client/src/Pages/Errors/NotFound.jsx
+++ client/src/Pages/Errors/NotFound.jsx
@@ -1,13 +1,28 @@
-import { Link } from "react-router-dom";
+import DinoImage from "/src/Assets/icons/gfgDino.png";
 
 const NotFound = () => {
   return (
-    <div className="px-16 md:px-24 py-16 flex flex-col items-center justify-center text-center">
-      <h1>404 - Page Not Found</h1>
-      <p>Sorry, the page you are looking for does not exist.</p>
-      <Link className="text-blue-600 underline" to="/">
-        Go to Home
-      </Link>
+    <div className="min-h-screen flex items-center justify-center bg-white p-4">
+      <div className="text-center max-w-3xl w-full">
+        <img 
+          src={DinoImage} 
+          alt="Cute dinosaur" 
+          className="w-[500px] h-[300px] mx-auto my-6" 
+        />
+        
+        <p className="text-gray-800 text-lg md:text-xl mb-8">
+          "Oh, it's you again... 😉"
+          <br />
+          Why are you back here? Shoo shoo, go fix it already!
+        </p>
+        
+        <a 
+          href="/" 
+          className=" bg-gfgsc-green text-white px-6 py-3 rounded inline-block text-base"
+        >
+          Go Home
+        </a>
+      </div>
     </div>
   );
 };
